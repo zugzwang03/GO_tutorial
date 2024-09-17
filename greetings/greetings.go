d52c1a4b22c42b5a3257dc1b8c8eecf16fdd63ff
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-09-17 11:05:19 +0530

    error handling and returning errors

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+
+	"errors"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+func Hello(name string) (string, error) {
+    // If no name is given, return an empty string with a valid error message
+    if name == "" {
+        return "", errors.New("empty name")
+    } 
+
+	// Otherwise return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
 }
