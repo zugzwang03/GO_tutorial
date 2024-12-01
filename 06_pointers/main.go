ff1b11c6c440a502213dc25570ee1ba40a656219
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-12-01 20:25:06 +0530

    learnt about pointers

---
+++ 06_pointers/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to the class of pointers")
+
+	// var ptr *int
+	// fmt.Println("Value of pointer is ", ptr)
+
+	myNumber := 23
+
+	var ptr = &myNumber
+	fmt.Println("Value of actual pointer memory address is", ptr)
+	fmt.Println("Value of pointer is", *ptr)
+
+	*ptr = *ptr + 2
+	fmt.Println("New value is: ", myNumber)
+}
