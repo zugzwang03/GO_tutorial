33512d5848230ce938238fe82e052bbd3dd33b76
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-12-08 18:11:49 +0530

    worked on controllers

--- 24_buildapi/main.go
+++ 24_buildapi/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // model for course - file
 type Course struct {
@@ -25,4 +31,35 @@ func (c *Course) isEmpty() bool {
 
 func main() {
 	fmt.Println("Welcome to building apis in golang")
+}
+
+// controllers - file
+// serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to API</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from request
+	params := mux.Vars(r)
+
+	// loop through the courses, find matching id and return the response
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No Course found with given id")
+	return
 }
