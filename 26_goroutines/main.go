59ddeb990dad8d3834dc87e30a0fdb347d5e0766
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-12-10 22:01:52 +0530

    learnt about wait groups

--- 26_goroutines/main.go
+++ 26_goroutines/main.go
@@ -2,17 +2,43 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"sync"
 	"time"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	go greeter("Hello")
 	greeter("world")
+	websitelist := []string{
+		"https://google.com",
+		"https://go.dev",
+		"https://google.in",
+		"https://fb.com",
+		"https://github.com",
+	}
+	for _, web := range websitelist {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+	wg.Wait()
 }
 
 func greeter(s string) {
 	for i := 0; i < 6; i++ {
 		time.Sleep(3 * time.Millisecond)
 		fmt.Println(s)
+	}
+}
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+	res, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("something wrong with endpoint")
+	} else {
+		fmt.Printf("%d status code for %s\n", res.StatusCode, endpoint)
 	}
 }
